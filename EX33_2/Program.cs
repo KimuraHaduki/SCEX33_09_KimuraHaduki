060ec7b77927ac67f10f2a7030f82bb7cce198bb
miso umisati <umisati4925@gmail.com>
2022-12-15 17:20:26 +0900

    プロジェクト ファイルを追加します。

---
+++ EX33_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EX33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectAngle rectAngle1 = new RectAngle(2, 3);
+            RectAngle rectAngle2 = new RectAngle(1, 4);
+            if (rectAngle1 == rectAngle2)
+            {
+                Console.WriteLine("rectAngle1とrectAngle2は等しい");
+            }
+            else
+            {
+                Console.WriteLine("rectAngle1とrectAngle2は等しくない");
+            }
+            RectAngle rectAngle3 = rectAngle1 + rectAngle2;
+            Console.WriteLine(rectAngle3.width + " : " + rectAngle3.height);
+        }
+    }
+}
---
+++ EX33_2/RectAngle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX33
+{
+    class RectAngle : ISurface, ICircumference, IBounds
+    {
+        readonly public float width;
+        readonly public float height;
+        public RectAngle(float width = 0, float height = 0)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public float GetSurface()
+        {
+            return width * height;
+        }
+        public float GetCircumference()
+        {
+            return (width + height) * 2;
+        }
+        public void GetBounds(out float width, out float height)
+        {
+            width = this.width;
+            height = this.height;
+        }
+        public static bool operator ==(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            return rectAngle1.width == rectAngle2.width
+                && rectAngle1.height == rectAngle2.height
+                || rectAngle1.width == rectAngle2.height
+                && rectAngle1.height == rectAngle2.width;
+        }
+
+        public static bool operator !=(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            return !(rectAngle1 == rectAngle2);
+        }
+        public static RectAngle operator +(RectAngle rectAngle1, RectAngle rectAngle2)
+        {
+            float newWidth;
+            float newHeight;
+            if (rectAngle1.width > rectAngle1.height)
+            {
+                rectAngle1 = new RectAngle(rectAngle1.height, rectAngle1.width);
+            }
+            if (rectAngle2.width > rectAngle2.height)
+            {
+                rectAngle2 = new RectAngle(rectAngle2.height, rectAngle2.width);
+            }
+            if (rectAngle1.width + rectAngle2.width < rectAngle1.height + rectAngle2.height)
+            {
+                newWidth = rectAngle1.width + rectAngle2.width;
+                if (rectAngle1.height > rectAngle2.height)
+                {
+                    newHeight = rectAngle1.height;
+                }
+                else
+                {
+                    newHeight = rectAngle2.height;
+                }
+            }
+            else
+            {
+                newHeight = rectAngle1.height + rectAngle2.height;
+                if (rectAngle1.width > rectAngle2.width)
+                {
+                    newWidth = rectAngle1.width;
+                }
+                else
+                {
+                    newWidth = rectAngle2.width;
+                }
+
+            }
+            return new RectAngle(newWidth, newHeight);
+        }
+    }
+}
